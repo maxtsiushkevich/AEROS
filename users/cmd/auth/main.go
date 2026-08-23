e78e87155244bde30a09c59d2b5c6f84a4ad893b
Max Tsiushkevich <max.tyushkevich@gmail.com>
2026-08-23 23:54:05 +0300

    Improved Users and Auth services

--- users/cmd/auth/main.go
+++ users/cmd/auth/main.go
@@ -1,8 +1,24 @@
 package main
 
-import "users/internal/http"
+import (
+	"flag"
+	"fmt"
+
+	"users/internal/config"
+	"users/internal/http"
+)
+
+var configPath = flag.String("config", "config/config.yaml", "Path to configuration file")
 
 func main() {
-	srv := http.NewServer()
+
+	flag.Parse()
+	cfg, err := config.Load(*configPath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	srv := http.NewServer(&cfg)
 	srv.ConfigServer()
 }
--- users/internal/http/server.go
+++ users/internal/http/server.go
@@ -4,23 +4,29 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+
+	"users/internal/config"
 )
 
 type Server struct {
 	router *gin.Engine
+	config *config.Config
 }
 
-func NewServer() *Server {
+func NewServer(cfg *config.Config) *Server {
 	return &Server{
 		router: gin.Default(),
+		config: cfg,
 	}
 }
 
 func (s *Server) ConfigServer() {
 	s.configRoutes()
 	server := &http.Server{
-		Addr:    ":3003",
-		Handler: s.router,
+		Addr:        s.config.HTTPServer.Address,
+		ReadTimeout: s.config.HTTPServer.Timeout,
+		IdleTimeout: s.config.HTTPServer.IdleTimeout,
+		Handler:     s.router,
 	}
 
 	server.ListenAndServe()
