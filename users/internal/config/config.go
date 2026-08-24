0a6b446600e52849ab59089795e88541a09be020
Tsiushkevich Maksim <maksim.tsiushkevich@appsys.net>
2026-08-24 16:48:47 +0300

    Added gRPC client to Users service. Updated config file

--- users/internal/config/config.go
+++ users/internal/config/config.go
@@ -19,6 +19,7 @@ type Config struct {
 	Env        Env              `yaml:"env"`
 	Database   DbConfig         `yaml:"database"`
 	HTTPServer HTTPServerConfig `yaml:"http_server"`
+	AuthServer AuthServerConfig `yaml:"auth_server"`
 }
 
 type DbConfig struct {
@@ -32,6 +33,10 @@ type HTTPServerConfig struct {
 	Address     string        `yaml:"address"`
 	Timeout     time.Duration `yaml:"timeout"`
 	IdleTimeout time.Duration `yaml:"idle_timeout"`
+}
+
+type AuthServerConfig struct {
+	Address string `yaml:"address"`
 }
 
 func Load(filePath string) (Config, error) {
--- users/internal/http/server.go
+++ users/internal/http/server.go
@@ -1,16 +1,24 @@
 package http
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
 
+	auth "users/api/proto"
+
 	"github.com/gin-gonic/gin"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"users/internal/config"
 )
 
 type Server struct {
-	router *gin.Engine
-	config *config.Config
+	router   *gin.Engine
+	config   *config.Config
+	grpcConn *grpc.ClientConn
 }
 
 func NewServer(cfg *config.Config) *Server {
@@ -21,7 +29,15 @@ func NewServer(cfg *config.Config) *Server {
 }
 
 func (s *Server) ConfigServer() {
+	authServerAddr := s.config.AuthServer.Address
+	conn, err := grpc.NewClient(authServerAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Error creating gRPC client: %v", err)
+	}
+	s.grpcConn = conn
+
 	s.configRoutes()
+
 	server := &http.Server{
 		Addr:        s.config.HTTPServer.Address,
 		ReadTimeout: s.config.HTTPServer.Timeout,
@@ -35,6 +51,21 @@ func (s *Server) ConfigServer() {
 
 func (s *Server) configRoutes() {
 	s.router.GET("/ping", func(c *gin.Context) {
+		client := auth.NewAuthClient(s.grpcConn)
+
+		resp, err := client.AddUser(context.Background(), &auth.AddUserRequest{
+			Id:             "1234",
+			HashedPassword: "34mf9304mf3940fj43jf34iksdz",
+		})
+
+		if err != nil {
+			c.String(http.StatusInternalServerError, "Error calling gRPC service: %v", err)
+			return
+		}
+
+		// В resp будут refresh и access токены, которые нужно записать в куки
+		fmt.Println(resp)
+
 		c.String(http.StatusOK, "pong")
 	})
 }
